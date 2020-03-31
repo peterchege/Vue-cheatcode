4280a535a28b393ffbb092c872a1bfef4a0a2e2c
peter chege <peterchege442@gmail.com>
2020-03-31 11:58:37 +0300

    first commmit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+    
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function(){
+            return{
+                quote:[
+                    "this is my quote test"
+                ],
+                maxQuotes:10
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
+ 
