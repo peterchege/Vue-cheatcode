8775195c627a8a34e1d4fe5c8691ed8b563f71d8
peter chege <peterchege442@gmail.com>
2020-04-01 22:48:01 +0300

    checkboxes in forms

--- forms/src/App.vue
+++ forms/src/App.vue
@@ -50,13 +50,15 @@
                             <input
                                     type="checkbox"
                                     id="sendmail"
-                                    value="SendMail"> Send Mail
+                                    value="SendMail"
+                                    v-model="sendMail"> Send Mail
                         </label>
                         <label for="sendInfomail">
                             <input
                                     type="checkbox"
                                     id="sendInfomail"
-                                    value="SendInfoMail"> Send Infomail
+                                    value="SendInfoMail"
+                                    v-model="sendMail"> Send Infomail
                         </label>
                     </div>
 
@@ -111,7 +113,7 @@
                         <p>Message:{{ message }} </p>
                         <p><strong>Send Mail?</strong></p>
                         <ul>
-                            <li></li>
+                            <li v-for=" item in sendMail"> {{ item }}</li>
                         </ul>
                         <p>Gender:</p>
                         <p>Priority:</p>
@@ -132,7 +134,8 @@
                     password: '',
                     age: 26
                     },
-                    message:'New text'
+                    message:'New text',
+                    sendMail:[]
                 
                 }
 
