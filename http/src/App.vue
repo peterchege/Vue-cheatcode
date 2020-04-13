a7e0814f48377077daf7d5d43cf195a417fa71b3
peter chege <peterchege442@gmail.com>
2020-04-13 18:28:35 +0300

    refactoring by making vue resourses global

--- http/src/App.vue
+++ http/src/App.vue
@@ -38,7 +38,7 @@
         },
         methods: {
             submit(){
-                this.$http.post('https://vuejs-http-80597.firebaseio.com/data.json', this.user)
+                this.$http.post('', this.user)
                         .then(response=>{
                             console.log(response)
                         }, error=>{
@@ -46,7 +46,7 @@
                         });
             },
             fetchData(){
-                this.$http.get('https://vuejs-http-80597.firebaseio.com/data.json')
+                this.$http.get('')
                     .then(response =>{
                              return response.json();
                         })
