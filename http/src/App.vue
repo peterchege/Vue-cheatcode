15e76c265409fc0930944e6de09a74aa0d2e1fcc
peter chege <peterchege442@gmail.com>
2020-04-13 17:39:17 +0300

    post request working with firebase

--- http/src/App.vue
+++ http/src/App.vue
@@ -31,7 +31,12 @@
         },
         methods: {
             submit(){
-                console.log(this.user)
+                this.$http.post('https://vuejs-http-80597.firebaseio.com/data.json', this.user)
+                        .then(response=>{
+                            console.log(response)
+                        }, error=>{
+                            console.log(error);
+                        });
             }
         },
     }
