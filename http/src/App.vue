ff610d82b72fa7580a0ce268527a561a66fed0dc
peter chege <peterchege442@gmail.com>
2020-04-13 17:07:14 +0300

    form temlate

--- http/src/App.vue
+++ http/src/App.vue
@@ -3,6 +3,16 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Http</h1>
+
+                <div class="form-group">
+                  <label for="">username</label>
+                  <input type="text" name="" class="form-control" v-model="user.username">
+                </div>
+                <div class="form-group">
+                  <label for="">mail</label>
+                  <input type="text" name="" class="form-control" v-model="user.email">
+                </div>
+                <button class="btn btn-primary" @click="submit">Submit</button>
             </div>
         </div>
     </div>
@@ -10,6 +20,20 @@
 
 <script>
     export default {
+        data(){
+            return {
+                user:{
+                    username: '',
+                    email: ''
+
+                }
+            }
+        },
+        methods: {
+            submit(){
+                console.log(this.user)
+            }
+        },
     }
 </script>
 
