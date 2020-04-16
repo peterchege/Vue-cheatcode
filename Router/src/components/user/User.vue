9adaf9ca9f9da5f43ad926aaeb9fe4b2256bb959
peter chege <peterchege442@gmail.com>
2020-04-16 21:27:31 +0300

    adding a button on a navbar

--- Router/src/components/user/User.vue
+++ Router/src/components/user/User.vue
@@ -1,4 +1,16 @@
 <template>
     <h1>The User Page</h1>
+    <hr>
+    <button @click="navigateToHome" class="btn btn-primary">Go to Home</button>
 
 </template>
+
+<script>
+export default {
+    methods: {
+        navigateToHome(){
+            this.$router.push('/');
+        }
+    }
+}
+</script>
