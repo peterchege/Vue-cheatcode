0f9362db31d5de21dd1effe985fab92d3943184f
peter chege <peterchege442@gmail.com>
2020-04-16 21:03:52 +0300

    linking using router-link

---
+++ Router/src/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+    <ul class="nav nav-pills">
+        <li role="presentation">
+             <router-link to="/"> Home</router-link>
+        </li>
+        <li role="presentation">
+            <router-link to="/user">User</router-link>
+        </li>
+    </ul>
+</template>
