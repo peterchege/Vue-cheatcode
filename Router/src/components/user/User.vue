dd87850d6f265072d58b56b8bbad7f776f1f0ba6
peter chege <peterchege442@gmail.com>
2020-04-17 15:46:11 +0300

    dynamic passing route id

--- Router/src/components/user/User.vue
+++ Router/src/components/user/User.vue
@@ -1,12 +1,20 @@
 <template>
-    <h1>The User Page</h1>
-    <hr>
-    <button @click="navigateToHome" class="btn btn-primary">Go to Home</button>
-
+    <div>
+         <h1>The User Page</h1>
+        <hr>
+        <p>Loaded ID: {{ id }} </p>
+        <button @click="navigateToHome" class="btn btn-primary">Go to Home</button>
+    </div>
+   
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            id: this.$route.params.id
+        }
+    },
     methods: {
         navigateToHome(){
             this.$router.push('/');
