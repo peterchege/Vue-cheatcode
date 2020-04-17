4c46369be561809a1f3808f8a3578fedcb386bba
peter chege <peterchege442@gmail.com>
2020-04-17 15:53:01 +0300

    using watch to dynamically change

--- Router/src/components/Header.vue
+++ Router/src/components/Header.vue
@@ -1,6 +1,7 @@
 <template>
     <ul class="nav nav-pills">
         <router-link to="/" tag="li" active-class ="active" exact> <a>Home</a></router-link>
-        <router-link to="/user/10" tag="li" active-class="active"><a>User</a></router-link>
+        <router-link to="/user/10" tag="li" active-class="active"><a>User1</a></router-link>
+        <router-link to="/user/20" tag="li" active-class="active"><a>User2</a></router-link>
     </ul>
 </template>
