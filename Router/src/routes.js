40b16cebb1e6a010bdade51700f3d3e4d9a529f6
peter chege <peterchege442@gmail.com>
2020-04-19 23:17:32 +0300

    * which handles all redirection

--- Router/src/routes.js
+++ Router/src/routes.js
@@ -18,5 +18,6 @@ export const routes = [
         { path:':id', component:UserDetail },
         { path:':id/edit', component:UserEdit, name:'userEdit' }
     ]},
-    { path:'/redirect-me', redirect:'/User'}
+    { path:'/redirect-me', redirect:{ name:'home'} },
+    { path:'*', redirect:'/'}
 ]; 
