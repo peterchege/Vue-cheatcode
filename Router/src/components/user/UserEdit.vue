ad1cbdc965585a3333e899b4ee34bcab605b1694
peter chege <peterchege442@gmail.com>
2020-04-19 23:59:31 +0300

    navigation guard

--- Router/src/components/user/UserEdit.vue
+++ Router/src/components/user/UserEdit.vue
@@ -3,6 +3,7 @@
         <h3>Edit the User</h3>
         <p>Locale: {{ $route.query.locale }}</p>
         <p>Analytics: {{ $route.query.q }}</p>
+        <button class="btn btn-primary"> confirm</button>
         <div style="height: 700px"></div>
         <p id="data">Some extra data</p>
     </div>
