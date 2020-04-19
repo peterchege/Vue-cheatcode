102520a353d70fe6ff6036a9c94004fa7b7844da
peter chege <peterchege442@gmail.com>
2020-04-19 23:53:42 +0300

    hashed fixed by pasting on the main div

--- Router/src/components/user/UserEdit.vue
+++ Router/src/components/user/UserEdit.vue
@@ -3,7 +3,8 @@
         <h3>Edit the User</h3>
         <p>Locale: {{ $route.query.locale }}</p>
         <p>Analytics: {{ $route.query.q }}</p>
+        <div style="height: 700px"></div>
+        <p id="data">Some extra data</p>
     </div>
-    <div style="height: 700px"></div>
-    <p id="data">Some extra data</p>
+   
 </template>
