3e06c3f6f4365a6ee4bbf575f054ac0e6f4576b1
peter chege <peterchege442@gmail.com>
2020-04-21 21:05:50 +0300

    another getter function

--- vuex/src/components/AnotherResult.vue
+++ vuex/src/components/AnotherResult.vue
@@ -1,5 +1,9 @@
 <template>
-    <p>Counter is: {{ counter }}</p>
+    <div>
+        <p>Counter is: {{ counter }}</p>
+        <p>Number of clicks: {{ clicks }}</p>
+    </div>
+    
 </template>
 
 <script>
@@ -7,6 +11,9 @@
        computed: {
            counter(){
                return this.$store.getters.doubleCounter;
+           },
+           clicks(){
+               return this.$store.getters.stringCounter;
            }
        },
     }
