a08858bb27add4239c46f3b75378a14283421b4a
peter chege <peterchege442@gmail.com>
2020-04-23 21:49:44 +0300

    restructuring using models

--- vuex/src/store/store.js
+++ vuex/src/store/store.js
@@ -5,27 +5,17 @@ Vue.use(Vuex);
 
 export const store = new Vuex.Store({
     state:{
-        counter: 0,
+        
         value: 0
     },
     getters:{
-        doubleCounter: state => {
-            return state.counter * 2;
-        },
-        stringCounter: state => {
-            return state.counter + ' clicks ';
-        },
+        
         value: state =>{
             return state.value;
         }
     },
     mutations: {
-        increment: (state, payload)  =>{
-            state.counter += payload;
-        },
-        decrement: (state, payload) =>{
-            state.counter -= payload;
-        },
+        
         updateValue: (state, payload) =>{
             state.value = payload;
         }
