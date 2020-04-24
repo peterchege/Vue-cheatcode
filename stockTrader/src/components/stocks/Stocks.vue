9f8729d53ce380d009b354906b7dc4b5d9c1e2d0
peter chege <peterchege442@gmail.com>
2020-04-24 22:18:37 +0300

    creating dataset for testing stocks

--- stockTrader/src/components/stocks/Stocks.vue
+++ stockTrader/src/components/stocks/Stocks.vue
@@ -1,3 +1,21 @@
 <template>
-    <h1> The Stocks component </h1>
+    <div>
+
+    </div>
 </template>
+
+<script>
+export default {
+    date(){
+        return{
+            stocks: [
+                { id:1, name:"BMW", price:110 },
+                { id:2, name:"Google", price:200 },
+                { id:3, name:"Apple", price:250 },
+                { id:4, name:"Twitter", price:8 },
+                
+            ]
+        }
+    }
+}
+</script>
