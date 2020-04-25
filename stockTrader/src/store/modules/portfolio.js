8a8e9841968e6981d95591cb8a62250e18e0de28
peter chege <peterchege442@gmail.com>
2020-04-25 19:02:50 +0300

    actions and getters

--- stockTrader/src/store/modules/portfolio.js
+++ stockTrader/src/store/modules/portfolio.js
@@ -26,4 +26,35 @@ const mutations = {
         }
         state.funds += stockPrice * quantity
     }
+};
+
+const actions = { 
+    sellStock({commit}, order){
+        commit('SELL_STOCK', order);
+    }
+};
+
+const getters = {
+    stockPortfolio (state, getters){
+        return state.stocks.map(stock => {
+            const record = getters.stocks.find(element => element.id == stock.id);
+            return {
+                id: stock.id,
+                quantity: stock.quantity,
+                name: record.name,
+                price: record.price
+            }
+
+        });
+    },
+    funds (state){
+         state.funds;
+    }
+};
+
+export default {
+    state,
+    mutations,
+    actions,
+    getters
 }
