8625372f7e633b437b6d6f118e0d0697004112cb
peter chege <peterchege442@gmail.com>
2020-04-26 23:56:31 +0300

    fixed the array stocks name

--- stockTrader/src/components/portfolio/Portfolio.vue
+++ stockTrader/src/components/portfolio/Portfolio.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
-        <app-stock v-for="stock in stock" :stock="stock"></app-stock>
+        <app-stock v-for="stock in stocks" :stock="stock"></app-stock>
     </div>
 </template>
 
 <script>
     import { mapGetter } from 'vue';
-    import Stock from './Stock.vue'
+    import Stock from './Stock.vue';
 
     export default {
         computed: {
