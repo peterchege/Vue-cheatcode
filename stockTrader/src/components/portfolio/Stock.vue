b38f177765633563e0f30588629b7d88a580fc8f
peter chege <peterchege442@gmail.com>
2020-04-28 23:05:37 +0300

    insufficient funds and quantity error checks

--- stockTrader/src/components/portfolio/Stock.vue
+++ stockTrader/src/components/portfolio/Stock.vue
@@ -14,20 +14,27 @@
                             class="form-control"
                             placeholder="Quantity"
                             v-model="quantity"
+                            class= "{ danger : insufficientQuantity}"
                             >
                 </div>
                     <div class="pull-right">
                         <button
                                 class="btn btn-success"
                                 @click="sellStock"
-                                :disabled="quantity <= 0 || !Number.isInteger(quantity) "
-                                >Sell
+                                :disabled="insufficientQuantity || quantity <= 0 || !Number.isInteger(quantity) "
+                                >{{ insufficientQuantity ? 'Not Enough ' : 'Sell'}}
                         </button>
                     </div>
             </div>    
         </div>
     </div>
 </template>
+
+<style scoped>
+    .danger {
+        border: 1px solid red;
+    }
+</style>
 
 <script>
     import { mapActions } from 'vuex';
@@ -36,6 +43,11 @@
         data() {
             return {
                 quantity: 0
+            }
+        },
+        computed: {
+            insufficientQuantity(){
+                return this.quantity > this.stock.quantity;
             }
         },   
         methods: {
--- stockTrader/src/components/stocks/Stock.vue
+++ stockTrader/src/components/stocks/Stock.vue
@@ -14,7 +14,7 @@
                             class="form-control"
                             placeholder="Quantity"
                             v-model="quantity"
-                            :class="{{ danger : insufficientFunds }}"
+                            :class="{ danger : insufficientFunds }"
                             >
                 </div>
                     <div class="pull-right">
