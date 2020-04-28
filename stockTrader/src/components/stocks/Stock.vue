90bcdb645d27a405871ac0fa171c93652a578159
peter chege <peterchege442@gmail.com>
2020-04-28 22:53:56 +0300

    insuffucient fund condition

--- stockTrader/src/components/stocks/Stock.vue
+++ stockTrader/src/components/stocks/Stock.vue
@@ -14,14 +14,15 @@
                             class="form-control"
                             placeholder="Quantity"
                             v-model="quantity"
+                            :class="{{ danger : insufficientFunds }}"
                             >
                 </div>
                     <div class="pull-right">
                         <button
                                 class="btn btn-success"
                                 @click="buyStock"
-                                :disabled="quantity <= 0 || !Number.isInteger(quantity) "
-                                >Buy
+                                :disabled="insufficientFunds || quantity <= 0 || !Number.isInteger(quantity) "
+                                >{{insufficientFunds ? 'Insufficient Funds' : 'Buy'}}
                         </button>
                     </div>
             </div>    
@@ -35,6 +36,14 @@
         data() {
             return {
                 quantity: 0
+            }
+        },
+        computed: {
+            funds(){
+                return this.$store.getters.funds;
+            },
+            insufficientFunds(){
+                return this.quantity * this.stock.price > this.funds;
             }
         },   
         methods: {
