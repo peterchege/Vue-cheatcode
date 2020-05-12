c72d93c624db45923ebd73f3a956b27d1fbb2cf9
peter chege <peterchege442@gmail.com>
2020-05-12 21:51:38 +0300

    pulling data and displaying using created live hooks

--- axios/src/components/dashboard/dashboard.vue
+++ axios/src/components/dashboard/dashboard.vue
@@ -2,15 +2,32 @@
   <div id="dashboard">
     <h1>That's the dashboard!</h1>
     <p>You should only get here if you're authenticated!</p>
+    <p>Your email address {{ email }} </p>
   </div>
 </template>
 
 <script>
   import axios from 'axios';
   export default {
+    data(){
+      return{
+        email:''
+      }
+    },
     created(){
       axios.get('https://axios-11287.firebaseio.com/user.json')
-      .then(res => console.log(res))
+      .then(res => {
+        const data = res.data;
+        users = [];
+        for(let key in data){
+          const user = data[key];
+          user.id = key;
+          users.push(user);
+        }
+        console.log(users)
+        this.email = users[0].email;
+        
+      })
       .catch(err => console.log(err))
     }
   }
