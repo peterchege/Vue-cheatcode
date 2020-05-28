a02f31b6412741fb4abcc6379fc157a69f6d1456
peter chege <peterchege442@gmail.com>
2020-05-28 11:19:35 +0300

    additional headers

--- axios/src/main.js
+++ axios/src/main.js
@@ -6,6 +6,8 @@ import router from './router'
 import store from './store'
 
 axios.defaults.baseURL = 'https://axios-11287.firebaseio.com'
+axios.defaults.headers.common['Authorization'] = 'Pksczdj342'
+axios.defaults.headers.get['Accepts'] = 'application/json'
 
 new Vue({
     el: '#app',
