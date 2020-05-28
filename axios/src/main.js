89d73f3a0aa541650f4d586a38a40275bf8fb7c4
peter chege <peterchege442@gmail.com>
2020-05-28 11:16:01 +0300

    refactoring using baseurl in axios

--- axios/src/main.js
+++ axios/src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
 
 import router from './router'
 import store from './store'
 
+axios.defaults.baseURL = 'https://axios-11287.firebaseio.com'
+
 new Vue({
-  el: '#app',
-  router,
-  store,
-  render: h => h(App)
+    el: '#app',
+    router,
+    store,
+    render: h => h(App)
 })
