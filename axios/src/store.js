a33cf44be3b36ce7f281b32496e60272ad8b8bb1
peterkinuthia <peterchege442@gmail.com>
2020-06-16 06:29:30 +0300

    updating th ui State(based on authentication)

--- axios/src/store.js
+++ axios/src/store.js
@@ -89,6 +89,9 @@ export default new Vuex.Store({
     getters: {
       user (state){
         return state.user
+      },
+      isAuthenticated (state) {
+        return state.idToken !== null
       }
     }
 })
