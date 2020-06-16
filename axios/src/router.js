e4f3a84cae6be442e020e5f18fb6acddbe7fd521
peterkinuthia <peterchege442@gmail.com>
2020-06-16 06:19:45 +0300

    protecting routes

--- axios/src/router.js
+++ axios/src/router.js
@@ -6,13 +6,25 @@ import DashboardPage from './components/dashboard/dashboard.vue'
 import SignupPage from './components/auth/signup.vue'
 import SigninPage from './components/auth/signin.vue'
 
+import store from './store'
+
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/', component: WelcomePage },
   { path: '/signup', component: SignupPage },
   { path: '/signin', component: SigninPage },
-  { path: '/dashboard', component: DashboardPage }
+  { 
+    path: '/dashboard', 
+    component: DashboardPage,
+    beforeEnter(to, from, next){
+      if (store.state.idToken){
+        next()
+      } else {
+        next('/signin')
+      }
+    }
+   }
 ]
 
 export default new VueRouter({mode: 'history', routes})
